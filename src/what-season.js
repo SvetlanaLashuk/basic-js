6d1f247496d2301460e0e72c820d255eccbb76ba
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-04-16 10:41:30 +0300

    feat: add what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -11,24 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // if (isNaN(date)) {
-  //   return 'Unable to determine the time of year!'
-  // } else if (Object.getOwnPropertyNames(date).length > 0) {
-  //   throw new Error('Invalid date!');
-  // } else {
-  //   let month = date.getMonth();
-  //   if (month >= 2 && month < 5) {
-  //     return 'spring';
-  //   } else if (month >= 5  && month < 8) {
-  //     return 'summer';
-  //   } else if ((month >= 8  && month < 11)) {
-  //     return 'autumn';
-  //   } else if (month = 11 || month >= 0 && month < 2) {
-  //     return 'winter';
-  //   }
-  // }
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  } else if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error('Invalid date!');
+  } else {
+    let month = date.getMonth();
+    if (month >= 2 && month < 5) {
+      return 'spring';
+    } else if (month >= 5  && month < 8) {
+      return 'summer';
+    } else if ((month >= 8  && month < 11)) {
+      return 'autumn';
+    } else if (month = 11 || month >= 0 && month < 2) {
+      return 'winter';
+    }
+  }
 }
 
 module.exports = {
